c5134e31553f7c46c2a0761b9e6f8693256be480
jon <jon@msi>
2012-11-01 17:15:31 -0400

    Added interactive graph interface

---
+++ src/webui/html/interactive/xtrace_utils.js
@@ -0,0 +1,35 @@
+var getReports = function(callback, errback) {
+	try {
+		var current_id = extractTraceID();
+	} catch(e) {
+		errback("Unable to extrace a traceID from URL", e);
+	}
+	getReportsForTrace(current_id, callback, errback);
+};
+
+var extractTraceID = function() {
+	var searchstring = window.location.search;
+	var id = searchstring.substr(searchstring.indexOf("id=")+3);
+	return id;
+};
+
+var getReportsForTrace = function(traceID, callback, errback) {
+	var report_url = "reports/" + traceID;
+	
+	var xhr = new XMLHttpRequest();
+	
+	xhr.open("GET", report_url, true);
+	
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState==4) {
+			if (xhr.status = 200) {
+				var json = JSON.parse(xhr.responseText);
+				callback(json);
+			} else {
+				errback(xhr.status, xhr);
+			}
+		}
+	};
+	
+	xhr.send(null);
+};
