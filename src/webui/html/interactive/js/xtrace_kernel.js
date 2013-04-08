fe7c2c5cef91063aa99f412756637360d585d7cf
Jonathan Mace <jonathan.c.mace@gmail.com>
2013-04-08 15:23:24 -0400

    Some tweaks and fixes.

--- src/webui/html/interactive/js/xtrace_kernel.js
+++ src/webui/html/interactive/js/xtrace_kernel.js
@@ -7,40 +7,39 @@ function XTraceCompareGraph(attach, reports) {
                                              .attr("viewBox", "0 0 " + w + " " + h )
                                              .attr("preserveAspectRatio", "xMidYMid meet");
     svg.node().oncontextmenu = function(d) { return false; };
-    
+
     // Extract the graphs we care about
     var graphs = reports.map(function(report) { return yarnchild_kernelgraph_for_trace(report); });
-    
+
     // Calculate pairwise distances
     var kernel = new NodeCountKernel();
     var edges = [];
     for (var i = 0; i < graphs.length; i++) {
         for (var j = 0; j < i; j++) {
-            edges.push({"source": i, "target": j, "score": kernel.calculate(graphs[i], graphs[j])});
+            edges.push({"source": i, "target": j, "score": kernel.calculate(graphs[i], graphs[j]), "weight": 0});
         }
     }
 
-    // Calculate the min, max, and mean
-    var min = edges.reduce(function(previousValue, currentValue) { 
-        return currentValue.weight < previousValue ? currentValue.weight : previousValue; 
-    }, Infinity);
+    // Calculate the min, max, and mean scores
+    var minEdge = edges.reduce(function(previousValue, currentValue) { 
+        return currentValue.score < previousValue.score ? currentValue : previousValue; 
+    }, {score: Infinity});
 
-    var max = edges.reduce(function(previousValue, currentValue) {
-        return currentValue.weight > previousValue ? currentValue.weight : previousValue;
-    }, 0);
-    
-    var mean = edges.reduce(function(previousValue, currentValue){
-        return previousValue+currentValue.weight;
+    var maxEdge = edges.reduce(function(previousValue, currentValue) {
+        return currentValue.score > previousValue.score ? currentValue : previousValue;
+    }, {score: 0});
+
+    var meanScore = edges.reduce(function(previousValue, currentValue){
+        return previousValue+currentValue.score;
     }, 0) / edges.length;
-    
-    // Normalize edge weights
-    if (min!=max) {
+
+    // Calculate the edge weights as being normalized scores
+    if (minEdge.score!=maxEdge.score) {
         edges.forEach(function(edge) {
-            edge.weight = 1 - (edge.weight - min) / (max - min);
-            console.log("edge weight is now", edge.weight);
+            edge.weight = 1 - Math.pow((edge.score) / (maxEdge.score), 2);
         })
     }
-    
+
     // Create the force layout
     var force = d3.layout.force().nodes(graphs).links(edges)
                   .on("tick", function() {
@@ -54,9 +53,11 @@ function XTraceCompareGraph(attach, reports) {
                        .attr("cy", function(d) { return d.y; });
                    })
                   .size([w, h])
-                  .linkDistance(function(edge) { return 20 + edge.weight*w/2; })
+                  .linkDistance(function(edge) { return 1 + edge.weight*w/2; })
+                  .linkStrength(function(edge) { return 1-edge.weight;})
                   .gravity(0.03)
                   .linkStrength(0.05)
+                  .charge(-10);
 
     force.start();
 
@@ -79,7 +80,6 @@ var yarnchild_kernelgraph_for_trace = function(trace) {
     // Create the full graph
     var graph = kernelgraph_for_trace(trace);
     
-    console.log(graph.get_nodes().length);
     
     // Find the process IDs for yarnchild processes
     var yarnchild_process_ids = {};
@@ -92,11 +92,10 @@ var yarnchild_kernelgraph_for_trace = function(trace) {
     
     // Remove any nodes that aren't yarnchild processes
     graph.get_nodes().forEach(function(node) {
-        if (!yarnchild_process_ids.hasOwnProperty(node.data["ProcessID"][0])) {
+        if (!node.data["ProcessID"] || !yarnchild_process_ids.hasOwnProperty(node.data["ProcessID"][0])) {
             graph.remove(node);
         }
     });
-    console.log(graph.get_nodes().length);
     
     return graph;
 }
