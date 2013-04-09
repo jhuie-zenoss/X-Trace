e359329899f65c067bae3e544dfaafce0ce5384d
Jonathan Mace <jonathan.c.mace@gmail.com>
2013-04-09 17:24:23 -0400

    Add tooltips and context menus to the visualization

--- src/webui/html/interactive/js/xtrace_kernel.js
+++ src/webui/html/interactive/js/xtrace_kernel.js
@@ -1,15 +1,19 @@
-function XTraceCompareGraph(attach, reports) {
+function XTraceCompareGraph(attach, data) {
     
     var w=1000, h=1000;
     
+    // Create the tooltip
+    var tooltip = CompareTooltip();
+    var ctxmenu = CompareGraphContextMenu();
+    
     // Twiddle the attach point a little bit
-    var svg = d3.select(attach).append("svg").attr("class", "compare-graph-attach")
+    var svg = d3.select(attach).append("svg").attr("class", "compare-graph")
                                              .attr("viewBox", "0 0 " + w + " " + h )
                                              .attr("preserveAspectRatio", "xMidYMid meet");
     svg.node().oncontextmenu = function(d) { return false; };
 
     // Extract the graphs we care about
-    var graphs = reports.map(function(report) { return yarnchild_kernelgraph_for_trace(report); });
+    var graphs = data.map(function(report) { return yarnchild_kernelgraph_for_trace(report); });
 
     // Calculate pairwise distances
     var kernel = new WeisfeilerLehmanKernel();
@@ -57,19 +61,44 @@ function XTraceCompareGraph(attach, reports) {
                   .linkStrength(function(edge) { return 1-edge.weight;})
                   .gravity(0.03)
                   .linkStrength(0.05)
-                  .charge(-10);
+                  .charge(-10)
+                  .alpha(0.5);
 
     force.start();
+    
+    function onNodeClick(d, e) {
+        if (!d3.event.ctrlKey) {
+            d3.selectAll(".node").classed("selected", false);
+        }
+        d3.select(this).classed("selected", !d3.select(this).classed("selected"));
+    }
 
     // Now actually draw the nodes and edges
-    svg.selectAll("line").data(edges).enter().append("line").attr("width", 1)
-                                                            .attr('stroke', "#BBB");
+    svg.selectAll(".edge").data(edges).enter().append("line").classed("edge", true);
     
-    svg.selectAll("circle").data(graphs).enter().append("circle").attr("fill", "#666")
-                                                                 .attr("r", 5)
-                                                                 .attr("cx", function() { return Math.random()*w; })
-                                                                 .attr("cy", function() { return Math.random()*h; })
-                                                                 .call(force.drag);
+    svg.selectAll(".node").data(graphs).enter().append("circle").classed("node", true)
+                           .attr("r", 10)
+                           .attr("cx", function() { return Math.random()*w; })
+                           .attr("cy", function() { return Math.random()*h; });
+    svg.selectAll(".node").call(tooltip)
+                          .call(force.drag)
+                          .on("click", onNodeClick);
+    
+    ctxmenu.call(svg, svg.selectAll(".node"));
+    ctxmenu.on("open", function() {
+        tooltip.hide();
+    }).on("view", function(d) {
+        window.open("graph.html?id="+d.get_id()+"&lightweight=true", "_blank");
+    }).on("hide", function(ds) {
+        ids = {};
+        graphs.map(function(d) {
+            ids[d.get_id()] = true;
+        })
+        ds.map(function(d) {
+            delete ids[d.get_id()];
+        })
+        window.open("compare.html?id="+Object.keys(ids).join(","),'_blank');
+    });
 }
 
 var kernelgraph_for_trace = function(trace) {
