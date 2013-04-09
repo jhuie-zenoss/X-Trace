c24cc79662d55369e5badf8b237d6cfdad148877
Jonathan Leavitt <jon.s.leavitt@gmail.com>
2013-04-09 12:52:49 -0400

    gitignore for target and data, TcpLocalDaemon file added, initial branch commit

---
+++ src/java/src/main/java/edu/berkeley/xtrace/server/TcpLocalDaemon.java
@@ -0,0 +1,15 @@
+public class TcpLocalDaemon {
+
+    private static final Logger LOG = Logger.getLogger(TcpLocalDaemon.class);
+    private static final int MAX_REPORT_LENGTH = 256*1024;
+
+    private int tcpport;
+
+    private BlockingQueue<String> q;
+
+    private ServerSocket serversock;
+
+
+
+
+}
