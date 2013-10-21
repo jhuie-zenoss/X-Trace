8aa437819c7ff7b76beb5c45c36a76f046bf59f1
Jonathan Mace <jonathan.c.mace@gmail.com>
2013-10-21 14:01:00 -0400

    Reverting resource tracing changes in test files

--- src/java/src/test/java/edu/berkeley/xtrace/XTraceTest.java
+++ src/java/src/test/java/edu/berkeley/xtrace/XTraceTest.java
@@ -8,29 +8,29 @@ public class XTraceTest extends TestCase {
 	
 	@Override
 	public void setUp() {
-		XTraceContext.doClearThreadContext();
+		XTraceContext.clearThreadContext();
 	}
 
 	@Test
 	public void testSingleContext() {
 		// null context
-		XTraceContext.doSetThreadContext((XTraceMetadata) null);
+		XTraceContext.setThreadContext((XTraceMetadata) null);
 		assertNull(XTraceContext.getThreadContext());
 		
 		// invalid context
-		XTraceContext.doSetThreadContext(new XTraceMetadata());
+		XTraceContext.setThreadContext(new XTraceMetadata());
 		assertNull(XTraceContext.getThreadContext());
 		
 		// valid context 1
 		TaskID task = new TaskID(4);
-		XTraceContext.doSetThreadContext(new XTraceMetadata(task, 1234));
+		XTraceContext.setThreadContext(new XTraceMetadata(task, 1234));
 		assertNotNull(XTraceContext.getThreadContext());
 		assertEquals(1, XTraceContext.getThreadContext().size());
 		assertEquals(new XTraceMetadata(task, 1234), XTraceContext.getThreadContext().iterator().next());
 		
 		// valid context 2
 		TaskID task2 = new TaskID(20);
-		XTraceContext.doSetThreadContext(new XTraceMetadata(task2, (long) 98769876));
+		XTraceContext.setThreadContext(new XTraceMetadata(task2, (long) 98769876));
 		assertNotNull(XTraceContext.getThreadContext());
 		assertEquals(1, XTraceContext.getThreadContext().size());
 		assertEquals(new XTraceMetadata(task2, (long) 98769876), XTraceContext.getThreadContext().iterator().next());
@@ -39,26 +39,26 @@ public class XTraceTest extends TestCase {
 	@Test
 	public void testMultiContext() {		
 		// null context 1
-		XTraceContext.doSetThreadContext((XTraceMetadata) null);
+		XTraceContext.setThreadContext((XTraceMetadata) null);
 		assertNull(XTraceContext.getThreadContext());
 		
 		// valid context 1
 		TaskID task = new TaskID(4);
-		XTraceContext.doSetThreadContext(new XTraceMetadata(task, 1234));
+		XTraceContext.setThreadContext(new XTraceMetadata(task, 1234));
 		assertNotNull(XTraceContext.getThreadContext());
 		assertEquals(1, XTraceContext.getThreadContext().size());
 		assertEquals(new XTraceMetadata(task, 1234), XTraceContext.getThreadContext().iterator().next());
 		assertTrue(XTraceContext.getThreadContext().contains(new XTraceMetadata(task, 1234)));
 		
 		// valid context 2
-		XTraceContext.doJoinContext(new XTraceMetadata(task, (long) 98769876));
+		XTraceContext.joinContext(new XTraceMetadata(task, (long) 98769876));
 		assertNotNull(XTraceContext.getThreadContext());
 		assertEquals(2, XTraceContext.getThreadContext().size());
 		assertTrue(XTraceContext.getThreadContext().contains(new XTraceMetadata(task, (long) 98769876)));
 
 		// valid context 3
 		TaskID task2 = new TaskID(20);
-		XTraceContext.doSetThreadContext(new XTraceMetadata(task2, (long) 123422));
+		XTraceContext.setThreadContext(new XTraceMetadata(task2, (long) 123422));
 		assertNotNull(XTraceContext.getThreadContext());
 		assertEquals(1, XTraceContext.getThreadContext().size());
 		assertEquals(new XTraceMetadata(task2, (long) 123422), XTraceContext.getThreadContext().iterator().next());		
@@ -69,10 +69,10 @@ public class XTraceTest extends TestCase {
 
 	@Test
 	public void testClearContext() {
-		XTraceContext.doSetThreadContext(new XTraceMetadata(new TaskID(12), 1234));
+		XTraceContext.setThreadContext(new XTraceMetadata(new TaskID(12), 1234));
 		assertNotNull(XTraceContext.getThreadContext());
 		assertEquals(1, XTraceContext.getThreadContext().size());
-		XTraceContext.doClearThreadContext();
+		XTraceContext.clearThreadContext();
 		assertNull(XTraceContext.getThreadContext());
 	}
 
@@ -93,9 +93,9 @@ public class XTraceTest extends TestCase {
 
 	@Test
 	public void testIsContextValid() {
-		XTraceContext.doClearThreadContext();
+		XTraceContext.clearThreadContext();
 		assertFalse(XTraceContext.isValid());
-		XTraceContext.doSetThreadContext(new XTraceMetadata(new TaskID(4), 1234));
+		XTraceContext.setThreadContext(new XTraceMetadata(new TaskID(4), 1234));
 		assertTrue(XTraceContext.isValid());
 	}
 
