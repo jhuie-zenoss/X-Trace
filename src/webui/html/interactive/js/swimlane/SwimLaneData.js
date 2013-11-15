334b8271d5c05e0df5c8e8893685cef548cf4d60
Jonathan Mace <jonathan.c.mace@gmail.com>
2013-11-14 20:17:32 -0500

    Added tooltip for the lanes to show thread/process info, and removed some unnecessary info that was cluttering the event tooltips

--- src/webui/html/interactive/js/swimlane/SwimLaneData.js
+++ src/webui/html/interactive/js/swimlane/SwimLaneData.js
@@ -38,7 +38,7 @@ var Workload = function(data, gcdata) {
 			}
 		}
 	}
-}
+};
 
 Workload.prototype.Tasks = function() {
 	var tasks = this.tasks;
@@ -46,44 +46,44 @@ Workload.prototype.Tasks = function() {
 	var values = keys.map(function(k) { return tasks[k]; });
 	values.sort(function(a, b) { return a.Start() - b.Start(); });
 	return values;	
-}
+};
 
 Workload.prototype.Machines = function() {
 	return [].concat.apply([], this.Tasks().map(function(task) { return task.Machines(); }));   
-}
+};
 
 Workload.prototype.Processes = function() {
 	return [].concat.apply([], this.Tasks().map(function(task) { return task.Processes(); }));   
-}
+};
 
 Workload.prototype.Threads = function() {
 	var threads = [].concat.apply([], this.Tasks().map(function(task) { return task.Threads(); }));
 	return threads;
-}
+};
 
 Workload.prototype.Spans = function() {
 	return [].concat.apply([], this.Tasks().map(function(task) { return task.Spans(); }));   
-}
+};
 
 Workload.prototype.Events = function() {
 	return [].concat.apply([], this.Tasks().map(function(task) { return task.Events(); }));   
-}
+};
 
 Workload.prototype.Edges = function() {
 	return [].concat.apply([], this.Tasks().map(function(task) { return task.Edges(); }));
-}
+};
 
 Workload.prototype.GCEvents = function() {
 	return [].concat.apply([], this.Tasks().map(function(task) { return task.GCEvents(); }));
-}
+};
 
 Workload.prototype.HDDEvents = function() {
 	return this.Events().filter(function(event) { return event.report["Operation"] && event.report["Operation"][0].substring(0, 4)=="file"; });
-}
+};
 
 Workload.prototype.ID = function() {
 	return "Task("+this.id+")";
-}
+};
 
 var Task = function(data) {
 	// Copy the params
@@ -110,39 +110,39 @@ Task.prototype.Machines = function() {
 	var values = keys.map(function(k) { return machines[k]; });
 	values.sort(function(a, b) { return a.Start() - b.Start(); });
 	return values;
-}
+};
 
 Task.prototype.Processes = function() {
 	return [].concat.apply([], this.Machines().map(function(machine) { return machine.Processes(); }));   
-}
+};
 
 Task.prototype.Threads = function() {
 	return [].concat.apply([], this.Machines().map(function(machine) { return machine.Threads(); }));
-}
+};
 
 Task.prototype.Spans = function() {
 	return [].concat.apply([], this.Machines().map(function(machine) { return machine.Spans(); }));   
-}
+};
 
 Task.prototype.Events = function() {
 	return [].concat.apply([], this.Spans().map(function(span) { return span.Events(); }));   
-}
+};
 
 Task.prototype.Edges = function() {
 	return [].concat.apply([], this.Events().map(function(event) { return event.Edges(); }));
-}
+};
 
 Task.prototype.GCEvents = function() {
 	return [].concat.apply([], this.Machines().map(function(machine) { return machine.GCEvents(); }));
-}
+};
 
 Task.prototype.ID = function() {
 	return "Task("+this.id+")";
-}
+};
 
 Task.prototype.Start = function() {
 	return Math.min.apply(this, this.Machines().map(function(process) { return process.Start(); }));
-}
+};
 
 var Event = function(span, report) {
 	this.report = report;
@@ -160,7 +160,7 @@ var Event = function(span, report) {
 	}
 
 	this.span.thread.process.machine.task.reports_by_id[this.id] = this;
-}
+};
 
 Event.prototype.Edges = function() {
 	if (this.edges==null) {
@@ -171,30 +171,21 @@ Event.prototype.Edges = function() {
 					id: this.id+parents[i],
 					parent: this.span.thread.process.machine.task.reports_by_id[parents[i]],
 					child: this
-			}
+			};
 			if (edge.parent && edge.child) 
 				this.edges.push(edge);
 		}
 	}
 	return this.edges;    
-}
+};
 
 Event.prototype.Timestamp = function() {
 	return this.timestamp;
-}
-
-Event.prototype.Name = function() {
-	if (this.report["Name"] && this.report["Name"].length >= 1)
-		return this.report["Name"][0];
-}
-
-Event.prototype.ProcessName = function() {
-
-}
+};
 
 Event.prototype.ID = function() {
 	return this.fqid;
-}
+};
 
 var Span = function(thread, id, reports) {
 	this.thread = thread;
@@ -216,14 +207,6 @@ var Span = function(thread, id, reports) {
 
 Span.prototype.ID = function() {
 	return this.fqid;
-};
-
-Span.prototype.Name = function() {
-	for (var i = 0; i < this.events.length; i++) {
-		var eventname = this.events[i].Name();
-		if (eventname)
-			return eventname;
-	}
 };
 
 Span.prototype.Events = function() {
@@ -267,7 +250,7 @@ var Thread = function(process, id, reports) {
 			Wait.events[0].timestamp = Wait.events[0].timestamp - duration;
 			preWaitEndEvent.timestamp = Wait.events[0].timestamp; // modify the timestamp of the end event of the prior span
 			this.spans.push(Wait);
-			
+
 			// Fix start/end ts (a hack, whatever)
 			preWait.end = preWaitEndEvent.timestamp;
 			Wait.start = Wait.events[0].timestamp;
@@ -285,32 +268,44 @@ var Thread = function(process, id, reports) {
 	if (span.length > 0)
 		this.spans.push(new Span(this, this.spans.length, span));
 	this.spans.sort(function(a, b) { return a.Start() - b.Start(); });
-}
+};
 
 Thread.prototype.ID = function() {
 	return this.fqid;
-}
+};
 
-Thread.prototype.Name = function() {
-	for (var i = 0; i < this.spans.length; i++) {
-		var spanname = this.spans[i].Name();
-		if (spanname)
-			return spanname;
+Thread.prototype.ShortName = function() {
+	var defaultName = "Thread-"+this.id;
+	var names = {};
+	names[defaultName] = true;
+	var events = this.Events();
+	for (var i = 0; i < events.length; i++) {
+		if (events[i].report["ThreadName"])
+			names[events[i].report["ThreadName"][0]] = true;
+	}
+	delete names[defaultName];
+	var othernames = Object.keys(names);
+	if (othernames.length > 0) {
+		var selected = othernames[0];
+		if (selected.length > 20)
+			return selected.substring(0, 20)+"...";
+		else
+			return selected;
 	}
 	return "Thread-"+this.id;
-}
+};
 
 Thread.prototype.Spans = function() {
 	return this.spans;
-}
+};
 
 Thread.prototype.Events = function() {
 	return [].concat.apply([], this.Spans().map(function(span) { return span.Events(); }));
-}
+};
 
 Thread.prototype.Start = function() {
 	return Math.min.apply(this, this.Spans().map(function(span) { return span.Start(); }));
-}
+};
 
 var Process = function(machine, id, reports) {
 	this.machine = machine;
@@ -343,11 +338,11 @@ var Process = function(machine, id, reports) {
 	this.threads = {};
 	for (var thread_id in reports_by_thread)
 		this.threads[thread_id] = new Thread(this, thread_id, reports_by_thread[thread_id]);
-}
+};
 
 Process.prototype.ID = function() {
 	return this.fqid;
-}
+};
 
 Process.prototype.Threads = function() {
 	var threads = this.threads;
@@ -355,33 +350,23 @@ Process.prototype.Threads = function() {
 	var values = keys.map(function(k) { return threads[k]; });
 	values.sort(function(a, b) { return a.Start() - b.Start(); });
 	return values;
-}
-
-Process.prototype.Name = function() {
-	var events = this.Events();
-	for (var i = 0; i < events.length; i++) {
-		var spanprocess = events[i].ProcessName();
-		if (spanprocess)
-			return spanprocess;
-	}
-	return this.id;
-}
+};
 
 Process.prototype.Spans = function() {
 	return [].concat.apply([], this.Threads().map(function(thread) { return thread.Spans(); }));
-}
+};
 
 Process.prototype.Events = function() {
 	return [].concat.apply([], this.Threads().map(function(thread) { return thread.Events(); }));
-}
+};
 
 Process.prototype.Start = function() {
 	return Math.min.apply(this, this.Threads().map(function(thread) { return thread.Start(); }));
-}
+};
 
 Process.prototype.GCEvents = function() {
 	return this.gcevents;
-}
+};
 
 var Machine = function(task, id, reports) {
 	this.task = task;
@@ -393,11 +378,11 @@ var Machine = function(task, id, reports) {
 	this.processes = {};
 	for (var process_id in reports_by_process)
 		this.processes[process_id] = new Process(this, process_id, reports_by_process[process_id]);
-}
+};
 
 Machine.prototype.ID = function() {
 	return this.fqid;
-}
+};
 
 Machine.prototype.Processes = function() {
 	var processes = this.processes;
@@ -405,27 +390,27 @@ Machine.prototype.Processes = function() {
 	var values = keys.map(function(k) { return processes[k]; });
 	values.sort(function(a, b) { return a.Start() - b.Start(); });
 	return values;
-}
+};
 
 Machine.prototype.Threads = function() {
 	return [].concat.apply([], this.Processes().map(function(process) { return process.Threads(); }));
-}
+};
 
 Machine.prototype.Spans = function() {
 	return [].concat.apply([], this.Processes().map(function(process) { return process.Spans(); }));    
-}
+};
 
 Machine.prototype.Events = function() {
 	return [].concat.apply([], this.Processes().map(function(process) { return process.Events(); }));    
-}
+};
 
 Machine.prototype.Start = function() {
 	return Math.min.apply(this, this.Processes().map(function(process) { return process.Start(); }));
-}
+};
 
 Machine.prototype.GCEvents = function() {
 	return [].concat.apply([], this.Processes().map(function(process) { return process.GCEvents(); }));
-}
+};
 
 function group_reports_by_field(reports, field) {
 	var grouping = {};
@@ -452,8 +437,8 @@ var GCEvent = function(process, report) {
 	this.duration = Number(this.report["GcDuration"][0])-1;
 	this.end = this.start + this.duration;
 	this.name = this.report["GcName"][0];
-}
+};
 
 GCEvent.prototype.ID = function() {
 	return this.fqid;
-}
+};
